cb112504cf7a6aa48d2f23ebfa04075be7b3270a
Paulo <phc25@hotmail.com>
2022-02-21 23:35:54 -0300

    Deletando arquivos repetidos em merge.

--- app/src/main/java/com/wolking/fortnite/ui/shop/adapters/ItemAdapterListener.kt
+++
@@ -1,7 +0,0 @@
-package com.wolking.fortnite.ui.shop.adapters
-
-import com.wolking.fortnite.data.models.shop.model.Entries
-
-interface ItemAdapterListener {
-    fun itemSelected(entries: Entries)
-}
